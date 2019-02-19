be4008be7ae2fc4f8901e36b570912e1e2c8fcc3
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2019-02-19 10:51:44 -0300

    Meu primeiro commit

---
+++ CaixaEletronico/CaixaEletronico.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace caixaEletronico
+{
+    public class CaixaEletronico
+    {
+        public string sacar(int v)
+        {
+            int[] notas = { 100, 50, 20, 10 };
+            int []aux = new int[4];
+            string resultado = "";
+            int resto;
+
+            //SE O VALOR FOR = 0 ELE VAI DIZER QUE VALOR É INVÁLIDO;
+            if( v <= 0)
+            {
+                resultado = "Valor inválido";
+            }
+            for (int i = 0; i < notas.Length; i++)
+            {
+                if (v >= notas[i])
+                {
+                    //PEGA O VALOR E DIVIDE PELAS NOTAS QUE TEM NO ARRAY.
+                    //EXEMPLO: 150 >>>> 150 / 100(POSIÇÃO[0]) VAI E JOGA 1 PARA O ARRAY AUX;
+                    aux[i] = v / notas[i];
+                         v = v % notas[i]; // EXEMPLO: 150 % 100 vai pegar e armazena o 50 no 'V';  
+                }
+            }
+            // ESSE LAÇO VAI VERIFICAR A POSIÇÃO QUE ESTÁ DIFERENTE DE 0 PARA PODER "PRINTAR"
+            for(int i = 0; i < aux.Length; i++)
+            {
+
+                if (aux[i] != 0 && aux[i] >= 1)
+                {
+                    //EXEMPLO AUX[0] = 1(VAI FICAR nota de 100: 1") 
+                    resultado += "nota de "+ notas[i].ToString() +": "  + aux[i].ToString();
+                }
+            }
+            if (v <= 0)
+                return resultado;
+            else
+                return "Cédula indisponível";
+
+                return resultado;
+        }
+    }
+}
---
+++ CaixaEletronicoTeste/CaixaEletronicoTeste.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using caixaEletronico;
+
+namespace CaixaEletronicoTeste
+{
+    [TestClass]
+    public class CaixaEletronicoTeste
+    {
+        [TestMethod]
+        public void Retorna1_100()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(100);
+
+            //VAI RETORNAR DESSE MESMO JETO A QUANTIDADE DE NOTAS QUE QUER
+            Assert.AreEqual("nota de 100: 1", NotasUsadas);
+        }
+
+        [TestMethod]
+        public void Retorna2_100()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(200);
+
+            Assert.AreEqual("nota de 100: 2", NotasUsadas);
+     
+       }
+
+        [TestMethod]
+        public void Retorna1_50()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(50);
+
+            Assert.AreEqual("nota de 50: 1", NotasUsadas);
+        }
+
+        [TestMethod]
+        public void Retorna1_20()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(20);
+
+            Assert.AreEqual("nota de 20: 1", NotasUsadas);
+        }
+
+        [TestMethod]
+        public void Retorna1_100_1_50()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(150);
+
+            Assert.AreEqual("nota de 100: 1" + "nota de 50: 1", NotasUsadas);
+        }
+
+        [TestMethod]
+        public void Retorna1_100_1_50_1_20_1_10()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(180);
+
+            Assert.AreEqual("nota de 100: 1" + "nota de 50: 1" + 
+                            "nota de 20: 1" + "nota de 10: 1", NotasUsadas);
+        }
+
+        [TestMethod]
+        public void Retorna10_100()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(1000);
+
+            Assert.AreEqual("nota de 100: 10" , NotasUsadas);
+        }
+
+        [TestMethod]
+        public void Retorna10_100_1_50()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(1050);
+
+            Assert.AreEqual("nota de 100: 10" + "nota de 50: 1", NotasUsadas);
+        }
+
+        [TestMethod]
+        public void Retorna10_100_1_50_1_20_1_10()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(1080);
+
+            Assert.AreEqual("nota de 100: 10" + "nota de 50: 1" + "nota de 20: 1" + "nota de 10: 1", NotasUsadas);
+        }
+
+        [TestMethod]
+        public void Retorna8_100_1_50_1_10()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(860);
+
+            Assert.AreEqual("nota de 100: 8" + "nota de 50: 1" + "nota de 10: 1", NotasUsadas);
+        }
+
+        [TestMethod]
+        public void RetornaCedulaIndisponivel()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(0);
+
+            Assert.AreEqual("Valor inválido", NotasUsadas);
+        }
+
+        [TestMethod]
+        public void RetornaNegativo()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(-2);
+
+            Assert.AreEqual("Valor inválido", NotasUsadas);
+        }
+
+        [TestMethod]
+        public void RetornaSemCedula()
+        {
+            CaixaEletronico caixa1 = new CaixaEletronico();
+
+            string NotasUsadas = caixa1.sacar(133);
+
+            Assert.AreEqual("Cédula indisponível", NotasUsadas);
+        }
+       
+    }
+}
